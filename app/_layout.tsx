29def1c74c10277a233f7ffa40ae450ec7c54258
jnvillamor <jnvillamor@careerteam.com>
2026-05-27 23:32:28 +0800

    feat: query client provider.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,13 @@
 import { Slot } from 'expo-router';
+import { QueryProvider } from '@/ui/providers/QueryProvider';
 import { ThemeProvider } from '@/ui/theme/ThemeProvider';
 
 export default function RootLayout() {
   return (
-    <ThemeProvider>
-      <Slot />
-    </ThemeProvider>
+    <QueryProvider>
+      <ThemeProvider>
+        <Slot />
+      </ThemeProvider>
+    </QueryProvider>
   );
 }
